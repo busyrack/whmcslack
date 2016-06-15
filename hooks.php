efaa9a68ce0232bb0fc290550c59e8fd5cbd1455
T. Tran <t-tran@users.noreply.github.com>
2016-06-15 16:51:16 +0700

    Fix PHP 5.3 backward compatible

--- hooks.php
+++ hooks.php
@@ -5,7 +5,7 @@ function whmcslack_getconfig()
     $q = select_query('tbladdonmodules', 'setting, value', array('module' => 'whmcslack'));
     if (mysql_num_rows($q) == 0)
         return null;
-    $r = [];
+    $r = array();
     while ($d = mysql_fetch_array($q)) {
         $r[$d['setting']] = $d['value'];
     }
@@ -14,7 +14,7 @@ function whmcslack_getconfig()
 
 function whmcslack_call($webhookUrl, $data)
 {
-    $payload = ['payload' => json_encode($data)];
+    $payload = array('payload' => json_encode($data));
     $response = curlCall($webhookUrl, $payload);
 }
 
@@ -27,7 +27,7 @@ function whmcslack_ClientAdd($vars)
     if (!$conf['new_client'])
         return;
     $url = $CONFIG['SystemURL'] . '/' . $customadminpath . '/clientssummary.php?userid=' . $vars['userid'];
-    $data = [];
+    $data = array();
     $data['text'] = 'A new client has signed up! ' .
         '<' . $url . '|Click here> for details!';
     whmcslack_call($conf['webhook'], $data);
@@ -42,7 +42,7 @@ function whmcslack_InvoicePaid($vars)
     if (!$conf['new_invoice'])
         return;
     $url = $CONFIG['SystemURL'] . '/' . $customadminpath . '/invoices.php?action=edit&id=' . $vars['invoiceid'];
-    $data = [];
+    $data = array();
     $data['text'] = 'Invoice ' . $vars['invoiceid'] . ' has just been paid! ' .
         '<' . $url . '|Click here> for details!';
     whmcslack_call($conf['webhook'], $data);
@@ -65,7 +65,7 @@ function whmcslack_TicketAdminReply($vars)
 
 function whmcslack_GetUserDisplayName($userid)
 {
-    $result = select_query('tblclients', 'firstname, lastname, email', ['id' => $userid]);
+    $result = select_query('tblclients', 'firstname, lastname, email', array('id' => $userid));
     if (mysql_num_rows($result) == 0)
         return '?';
     $data = mysql_fetch_array($result);
@@ -82,15 +82,15 @@ function whmcslack_TicketChange($status, $vars)
     if ($conf == null || empty($conf['webhook']))
         return;
     $url = $CONFIG['SystemURL'] . '/' . $customadminpath . '/supporttickets.php?action=viewticket&id=' . $vars['ticketid'];
-    $data = [];
-    $attachement = [];
+    $data = array();
+    $attachement = array();
     $attachement['title'] = 'Ticket #' . $vars['ticketid'] . ': ' . $vars['subject'];
     $attachement['title_link'] = $url;
     $attachement['text'] = substr($vars['message'], 0, 50) . '…';
     $attachement['color'] = "#7CD197";
-    $attachement['fields'] = [];
-    $attachement['fields'][] = ['title' => 'Priority', 'value' => $vars['priority'], 'short' => false];
-    $attachement['fields'][] = ['title' => 'Department', 'value' => $vars['deptname'], 'short' => false];
+    $attachement['fields'] = array();
+    $attachement['fields'][] = array('title' => 'Priority', 'value' => $vars['priority'], 'short' => false);
+    $attachement['fields'][] = array('title' => 'Department', 'value' => $vars['deptname'], 'short' => false);
 
     switch ($status) {
         case 'user_reply':
@@ -112,7 +112,7 @@ function whmcslack_TicketChange($status, $vars)
             return;
     }
 
-    $data['attachments'] = [$attachement];
+    $data['attachments'] = array($attachement);
     whmcslack_call($conf['webhook'], $data);
 }
 
@@ -122,3 +122,5 @@ add_hook("InvoicePaid", 1, "whmcslack_InvoicePaid");
 add_hook("TicketOpen", 1, "whmcslack_TicketOpen");
 add_hook("TicketUserReply", 1, "whmcslack_TicketUserReply");
 add_hook("TicketAdminReply", 1, "whmcslack_TicketAdminReply");
+
+?>
--- whmcslack.php
+++ whmcslack.php
@@ -5,26 +5,26 @@ if (!defined('WHMCS'))
 
 function whmcslack_config()
 {
-    $configarray = [
+    $configarray = array(
         'name' => 'WHMCSlack',
         'description' => 'Receive a Slack notification for major event',
         'version' => '1.1',
         'author' => 'PunKeel',
         'language' => 'english',
-        'fields' => [
-            'webhook' => ['FriendlyName' => 'Slack Webhook', 'Type' => 'text', 'Size' => '80', 'Description' => '', 'Default' => 'https://hooks.slack.com/services/T012345AC/B0AT00A0A/...'],
-            'new_client' => ['FriendlyName' => 'Alert New Client', 'Type' => 'yesno', 'Size' => '25',
-                'Description' => ''],
-            'new_invoice' => ['FriendlyName' => 'Alert Invoice Paid', 'Type' => 'yesno', 'Size' => '25',
-                'Description' => ''],
-            'new_ticket' => ['FriendlyName' => 'Alert New Ticket', 'Type' => 'yesno', 'Size' => '25',
-                'Description' => ''],
-            'new_update' => ['FriendlyName' => 'Alert Ticket Update', 'Type' => 'yesno', 'Size' => '25',
-                'Description' => ''],
-            'new_update_admin' => ['FriendlyName' => 'Alert Ticket Admin Reply', 'Type' => 'yesno', 'Size' => '25',
-                'Description' => ''],
-        ]
-    ];
+        'fields' => array(
+            'webhook' => array('FriendlyName' => 'Slack Webhook', 'Type' => 'text', 'Size' => '80', 'Description' => '', 'Default' => 'https://hooks.slack.com/services/T012345AC/B0AT00A0A/...'),
+            'new_client' => array('FriendlyName' => 'Alert New Client', 'Type' => 'yesno', 'Size' => '25',
+                'Description' => ''),
+            'new_invoice' => array('FriendlyName' => 'Alert Invoice Paid', 'Type' => 'yesno', 'Size' => '25',
+                'Description' => ''),
+            'new_ticket' => array('FriendlyName' => 'Alert New Ticket', 'Type' => 'yesno', 'Size' => '25',
+                'Description' => ''),
+            'new_update' => array('FriendlyName' => 'Alert Ticket Update', 'Type' => 'yesno', 'Size' => '25',
+                'Description' => ''),
+            'new_update_admin' => array('FriendlyName' => 'Alert Ticket Admin Reply', 'Type' => 'yesno', 'Size' => '25',
+                'Description' => ''),
+        )
+    );
     return $configarray;
 }
 
